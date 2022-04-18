6f4081c357130c030bdc93d26d97515b348cf6b5
andre-carvalho <afacarvalho@yahoo.com.br>
2022-04-18 17:12:45 -0300

    Improve the build process

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VERSION=$(cat ./PROJECT_VERSION | grep -oP '(?<="version": ")[^"]*')
+VERSION=$(git describe --tags --abbrev=0)
 export VERSION
 
 # build all images
-docker build -t terrabrasilis/backup-pg-dump:v$VERSION --build-arg VERSION=$VERSION -f Dockerfile ./src
+docker build -t terrabrasilis/backup-pg-dump:$VERSION --build-arg VERSION=$VERSION -f Dockerfile ./src
 
 # send to dockerhub
 echo "The building was finished! Do you want sending this new image to Docker HUB? Type yes to continue." ; read SEND_TO_HUB
@@ -12,5 +12,5 @@ if [[ ! "$SEND_TO_HUB" = "yes" ]]; then
     echo "Ok, not send the image."
 else
     echo "Nice, sending the image!"
-    docker push terrabrasilis/backup-pg-dump:v$VERSION
+    docker push terrabrasilis/backup-pg-dump:$VERSION
 fi
